82f780c3a11dfd88b8427cd9e36c9d7064c53f35
BuildTools <unconfigured@null.spigotmc.org>
2018-09-06 22:31:32 +1000

    messing with product page, testing alignment

--- a2/product.php
+++ a2/product.php
@@ -1,87 +1,93 @@
 <!DOCTYPE html>
 <html lang='en'>
-  <head>
-    <meta charset="utf-8">
-    <title>Christmas On Main</title>
+<head>
+  <meta charset="utf-8">
+  <title>Christmas On Main</title>
 
-    <!-- Keep wireframe.css for debugging, add your css to style.css -->
-    <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
-    <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css">
-    <script src='../wireframe.js'></script>
-  </head>
+  <!-- Keep wireframe.css for debugging, add your css to style.css -->
+  <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
+  <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css">
+  <script src='../wireframe.js'></script>
+</head>
 
-  <body>
+<body>
 
-    <header>
-      <div>
-		<!-- Creative Commons image sourced from https://facebook.com/christmasonmain77 and used for educational purposes only -->
-		<img src="../../media/ChristmasOnMain-logo.png" alt="Christmas On Main logo" class="center">
-	  </div>
-    </header>
+  <header>
+    <div>
+      <!-- Creative Commons image sourced from https://facebook.com/christmasonmain77 and used for educational purposes only -->
+      <img src="../../media/ChristmasOnMain-logo.png" alt="Christmas On Main logo" class="center">
+    </div>
+  </header>
 
-    <nav class="nav">
-	  <ul>
-			<li><a class="active" href="index.php">Home</a></li>
-			<li class="dropdown">
-				<a href="javascript:void(0)" class="dropbtn">Products</a>
-				<div class="dropdown-content">
-					<a href="products.php#trees">Trees</a>
-					<a href="products.php#decor">Decorations</a>
-					<a href="products.php#elves">Elves</a>
-				</div>
-			</li>
-			<li><a href="login.php">Login</a></li>
-		</ul>
-    </nav>
+  <nav class="nav">
+    <ul>
+      <li><a class="active" href="index.php">Home</a></li>
+      <li class="dropdown">
+        <a href="javascript:void(0)" class="dropbtn">Products</a>
+        <div class="dropdown-content">
+          <a href="products.php#trees">Trees</a>
+          <a href="products.php#decor">Decorations</a>
+          <a href="products.php#elves">Elves</a>
+        </div>
+      </li>
+      <li><a href="login.php">Login</a></li>
+    </ul>
+  </nav>
 
-    <main>
-	<!-- Creative Commons image sourced from https://facebook.com/christmasonmain77 and used for educational purposes only -->
-      <h1>Fir Christmas Tree</h1>
-      <div class="test">
-      <img src="http://placehold.it/350x350" />
-      <p>Size</p>
-      <form id="formproduct">
-        <label class="container">S
-          <input type="radio" checked="checked" name="radio">
+  <main>
+    <!-- Creative Commons image sourced from https://facebook.com/christmasonmain77 and used for educational purposes only -->
+    <h1>Fir Christmas Tree</h1>
+    <div class="test center">
+      <div id="previewimage">
+        <img src="http://placehold.it/350x350" />
+      </div>
+      <!-- <p>Size</p> -->
+      <div id="formproduct">
+        <form>
+          <label class="container">S
+            <input type="radio" checked="checked" name="radio">
             <span class="checkmark"></span>
-      </label>
-      <label class="container">M
-        <input type="radio" name="radio">
-        <span class="checkmark"></span>
-      </label>
-        <label class="container">L
+          </label>
+          <label class="container">M
             <input type="radio" name="radio">
             <span class="checkmark"></span>
-        </label>
-        <input class="center" id="purchasebutton" type="submit" value="Purchase">
-      </form>
+          </label>
+          <label class="container">L
+            <input type="radio" name="radio">
+            <span class="checkmark"></span>
+          </label>
+          <input class="center" id="purchasebutton" type="submit" value="Purchase">
+        </form>
+        <p>$xx.xx</p>
+      </div>
     </div>
-      <p>$xx.xx</p>
 
-      <h2>See Similar Products:</h2>
-      <div class="wrapper">
-  <div class="scrolls">
-  <div class="imageDiv">
-      <img src="http://placehold.it/250x250" />
-      <img src="http://placehold.it/250x250" />
-      <img src="http://placehold.it/250x250" />
-      <img src="http://placehold.it/250x250" />
-      <img src="http://placehold.it/250x250" />
-      <img src="http://placehold.it/250x250" />
-      <img src="http://placehold.it/250x250" />
+    <!-- <div id="similar"> -->
+    <div class="wrapper">
+      <div class="scrolls">
+        <h2>See Similar Products:</h2>
+        <div class="imageDiv">
+          <img src="http://placehold.it/250x250" />
+          <img src="http://placehold.it/250x250" />
+          <img src="http://placehold.it/250x250" />
+          <img src="http://placehold.it/250x250" />
+          <img src="http://placehold.it/250x250" />
+          <img src="http://placehold.it/250x250" />
+          <img src="http://placehold.it/250x250" />
+        </div>
+      </div>
     </div>
-  </div>
-  </div>
+  <!-- </div> -->
 
-    </main>
+  </main>
 
-    <footer>
-      <div>&copy;<script>
-        document.write(new Date().getFullYear());
-      </script> Dylan Sbogar: s3718036 & Matthew Moloney: s3717566.<br>"Group Name"</br></div>
-      <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
-      <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
-    </footer>
+  <footer>
+    <div>&copy;<script>
+    document.write(new Date().getFullYear());
+    </script> Dylan Sbogar: s3718036 & Matthew Moloney: s3717566.<br>"Group Name"</br></div>
+    <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
+    <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
+  </footer>
 
-  </body>
+</body>
 </html>

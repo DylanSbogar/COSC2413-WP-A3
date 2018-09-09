242a9438edc77c17c5015057db4519a819706d0d
BuildTools <unconfigured@null.spigotmc.org>
2018-09-09 14:11:02 +1000

    created the quantity incrementer/decrementer code

--- a2/quantity.js
+++ a2/quantity.js
@@ -1,1 +1,8 @@
-re
+var quantityvalue = 0;
+function incrementValue() {
+    document.getElementById('quantitynumber').value = ++quantityvalue;
+}
+
+function decrementValue() {
+    document.getElementById('quantitynumber').value = --quantityvalue;
+}

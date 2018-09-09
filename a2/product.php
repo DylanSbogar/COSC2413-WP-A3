3ed8212dce127d12ac20e22b4119b070704d468e
BuildTools <unconfigured@null.spigotmc.org>
2018-09-09 18:58:50 +1000

    neatened info and improved contrast, centered and cleaned.

--- a2/product.php
+++ a2/product.php
@@ -21,51 +21,47 @@
 
   <nav class="nav">
     <ul>
-      <li><a class="active" href="index.php">Home</a></li>
-      <li class="dropdown">
-        <a href="products.php" class="dropbtn">Products</a>
-        <div class="dropdown-content">
-          <a href="products.php#trees">Trees</a>
-          <a href="products.php#decor">Decorations</a>
-          <a href="products.php#elves">Elves</a>
-        </div>
-      </li>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="products.php">Products</a></li>
       <li><a href="login.php">Login</a></li>
     </ul>
   </nav>
 
   <main>
     <!-- Creative Commons image sourced from https://facebook.com/christmasonmain77 and used for educational purposes only -->
-    <h1>Fir Christmas Tree</h1>
-    <div class="test">
+    <div class="infobox">
       <!-- <p>Size</p> -->
       <div id="formproduct">
-        <img class="center" src="https://i.imgur.com/QEPurWz.jpg"/></img>
+        <img class="center" src="../../media/products/firGen.jpg"/></img>
       </div>
 
       <div id="information">
+        <h1>Genuine Fir Tree</h1>
         <form action="https://titan.csit.rmit.edu.au/~e54061/wp/processing.php?ref=product" method="post">
-          <span id="price"><p>$200.00</p></span>
           <input type="hidden" name="id" value="t001">
-          <label class="container">S
-            <input type="radio" checked="checked" name="option" value="small">
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">M
-            <input type="radio" name="option" value="medium">
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">L
-            <input type="radio" name="option" value="large">
-            <span class="checkmark"></span>
-          </label>
-
+          <div class="buttonsizes center">
+            <label class="container">S
+              <input type="radio" checked="checked" name="option" value="small">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">M
+              <input type="radio" name="option" value="medium">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">L
+              <input type="radio" name="option" value="large">
+              <span class="checkmark"></span>
+            </label>
+          </div>
           <br>
           <div id="inputbox">
+            <script src="quantity.js"></script>
+            <p id="quantity">quantity</p>
             <input type="button" value="-" onclick='decrementValue()' name="decrease"></input>
             <input id="quantitynumber" type="number" name="qty" readonly>
             <input type="button" value="+" onclick='incrementValue()' name="increase"></input>
           </div>
+          <span id="price"><p>$200.00 - $350.00</p></span>
           <input class="center" id="purchasebutton" type="submit" value="Purchase"></input>
         </form>
       </div>
@@ -75,42 +71,42 @@
 
     <h2>See Other Products:</h2>
     <div id="productgallery">
-  <!-- Creative Commons image sourced from https://facebook.com/christmasonmain77 and used for educational purposes only -->
-  <div class="responsive">
-  <div class="gallery">
-    <a href="product.php">
-    <img class="center" src="../../media/products/firArt.jpg"></img>
-    <div class="desc">
-      <span class="productname">Artificial Fir Tree</span> <!-- name -->
-      <hr>
-      <span>$200 - $350</span> <!-- price -->
-     </div>
-    </a>
-  </div>
-  </div>
+      <!-- Creative Commons image sourced from https://facebook.com/christmasonmain77 and used for educational purposes only -->
+      <div class="responsive">
+        <div class="gallery">
+          <a href="product.php">
+            <img class="center" src="../../media/products/firArt.jpg"></img>
+            <div class="desc">
+              <span class="productname">Artificial Fir Tree</span> <!-- name -->
+              <hr>
+              <span>$200 - $350</span> <!-- price -->
+            </div>
+          </a>
+        </div>
+      </div>
 
-  <div class="responsive">
-  <div class="gallery">
-    <img class="center" src="../../media/products/pineGen.jpg"></img>
-    <div class="desc">
-      <span class="productname">Genuine Pine Tree</span> <!-- name -->
-      <hr>
-      <span>$200 - $350</span> <!-- price -->
-     </div>
+      <div class="responsive">
+        <div class="gallery">
+          <img class="center" src="../../media/products/pineGen.jpg"></img>
+          <div class="desc">
+            <span class="productname">Genuine Pine Tree</span> <!-- name -->
+            <hr>
+            <span>$200 - $350</span> <!-- price -->
+          </div>
+        </div>
+      </div>
+      <div class="responsive">
+        <div class="gallery">
+          <img class="center" src="../../media/products/pineArt.jpg"></img>
+          <div class="desc">
+            <span class="productname">Artificial Pine Tree</span> <!-- name -->
+            <hr>
+            <span>$200 - $350</span> <!-- price -->
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-  <div class="responsive">
-  <div class="gallery">
-    <img class="center" src="../../media/products/pineArt.jpg"></img>
-    <div class="desc">
-      <span class="productname">Artificial Pine Tree</span> <!-- name -->
-      <hr>
-      <span>$200 - $350</span> <!-- price -->
-     </div>
-   </div>
-  </div>
-  </div>
-  <!-- </div> -->
+    <!-- </div> -->
 
   </main>
 

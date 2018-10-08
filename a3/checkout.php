8e63f2b42fdf7e3b532f9dcc1c8a5802c283d062
BuildTools <unconfigured@null.spigotmc.org>
2018-10-08 23:46:20 +1100

    made php code that reads in a comma delimited .txt and prints to a table, inserted dummy text

--- a3/checkout.php
+++ a3/checkout.php
@@ -12,17 +12,52 @@
 
 <body>
 
-<?php
-include 'include/header.php';
-?>
+  <?php
+  include 'include/header.php';
+  ?>
 
-<?php
-include 'include/nav.php';
- ?>
- 
-<?php
- include 'include/footer.php';
-?>
+  <?php
+  include 'include/nav.php';
+  ?>
+
+  <main>
+    <div>
+
+      <?php
+      $file = fopen('products.txt','r');
+      while ($line = fgets($file))
+      {
+        $records[] = explode(",", $line);
+      }
+      // print_r($records);
+      fclose($file);
+      // print_r($records[1][3]);
+
+
+      ?>
+      <table id="temptable">
+      <tr>
+      <td><?php echo $records[0][0] ?></td>
+      <td><?php echo $records[0][1] ?></td>
+      <td><?php echo $records[0][2] ?></td>
+      <td><?php echo $records[0][3] ?></td>
+      <td><?php echo $records[0][4] ?></td>
+      </tr>
+      <tr>
+      <td><?php echo $records[1][0] ?></td>
+      <td><?php echo $records[1][1] ?></td>
+      <td><?php echo $records[1][2] ?></td>
+      <td><?php echo $records[1][3] ?></td>
+      <td><?php echo $records[1][4] ?></td>
+      </tr>
+      </table>
+
+    </div>
+
+  </main>
+  <?php
+  include 'include/footer.php';
+  ?>
 
 </body>
 </html>
